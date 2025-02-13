a13085db611dcd9348c14e0a7437d5f621bd262d
Bernhard Thoben <berni-the-bear@web.de>
2025-02-13 14:49:41 +0100

    Missing header file added.
    This prevents error messages like these: "error: using directive refers to implicitly-defined namespace 'std' [-Werror] 32 | using namespace std;"
    
    Change-Id: Icd7633deadad50c5f3f6688c1ad8da5c60ba318d

--- mm-audio/aenc-evrc/qdsp6/inc/Map.h
+++ mm-audio/aenc-evrc/qdsp6/inc/Map.h
@@ -29,6 +29,7 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define _MAP_H_
 
 #include <stdio.h>
+#include <string>
 using namespace std;
 
 template <typename T,typename T2>
--- mm-audio/aenc-evrc/qdsp6/src/omx_evrc_aenc.cpp
+++ mm-audio/aenc-evrc/qdsp6/src/omx_evrc_aenc.cpp
@@ -35,7 +35,8 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //////////////////////////////////////////////////////////////////////////////
 
 
-#include<string.h>
+#include <string>
+#include <string.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include "omx_evrc_aenc.h"
